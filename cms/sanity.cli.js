2ddb49da77b7403dc842e8e156839818486b71db
jakobprufer <jakobo.pruefer@gmail.com>
2024-02-09 13:01:00 +0100

    Removed folder from tracking

--- cms/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'vlugq6ei',
-    dataset: 'production'
-  }
-})
--- cms/schemas/index.js
+++
@@ -1,5 +0,0 @@
-import blockContent from './blockContent'
-import caseStudy from './caseStudy'
-import project from './project'
-
-export const schemaTypes = [project, caseStudy, blockContent]
