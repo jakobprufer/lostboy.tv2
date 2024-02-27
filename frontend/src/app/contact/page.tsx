667b3a5fbf97dcddee6a03068c2d87ddfe666235
jakobprufer <jakobo.pruefer@gmail.com>
2024-02-27 19:38:04 +0100

    finished 2.0

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,11 +1,51 @@
+"use client";
 import React from "react";
+import { Zustand } from "../Zustand/Zustand";
 
 export default function Contact() {
+  //getting states from Zustand
+  const { mouseOverEvent, mouseOutEvent } = Zustand();
+
   return (
     <div className="pageContent medText">
-      <p>Let's work</p>
-      <p>Luca Antoniazzi</p>
-      <p></p>
+      <h1 className="stardom">contact</h1>
+      <div className="contactDetails">
+        <div>Luca Antoniazzi</div>
+        <div>
+          <a
+            href="mailto:luca@lostboy.tv"
+            onMouseOut={mouseOutEvent}
+            onMouseOver={mouseOverEvent}
+          >
+            luca@lostboy.tv
+          </a>
+          <div
+            onClick={() => navigator.clipboard.writeText("luca@lostboy.tv")}
+            className="contactButton"
+            onMouseOut={mouseOutEvent}
+            onMouseOver={mouseOverEvent}
+          >
+            Copy
+          </div>
+        </div>
+        <div>
+          <a
+            href="tel:00447847899041"
+            onMouseOut={mouseOutEvent}
+            onMouseOver={mouseOverEvent}
+          >
+            +44 7847 899041
+          </a>
+          <div
+            onClick={() => navigator.clipboard.writeText("+447847899041")}
+            className="contactButton"
+            onMouseOut={mouseOutEvent}
+            onMouseOver={mouseOverEvent}
+          >
+            Copy
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
