e81af3afa4bf9b6e9f4ac631b631e65ef6c32e9d
jakobprufer <jakobo.pruefer@gmail.com>
2024-03-10 21:52:02 +0100

    trying fetch revalidation on homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,8 @@ import WorkGrid from "./Components/WorkGrid";
 import { getProjects } from "./SanityUtils";
 import HomeContent from "./Components/HomeContent";
 
+export const revalidate = 10;
+
 export default async function Home() {
   //get data
   const projects = await getProjects();
